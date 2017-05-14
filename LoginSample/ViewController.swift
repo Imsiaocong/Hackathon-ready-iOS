b924a34bd5eaabf6d4a7ba24e866e0b992f7fdb4
王笛 Wang,Di <wd1998313@gmail.com>
2017-05-14 15:51:14 -0400

    Initial Commit

---
+++ LoginSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginSample
+//
+//  Created by Di Wang on 2017/5/14.
+//  Copyright © 2017年 Di Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
